97b75d97318a05a2bf8b7b477d95c0e4c66ff9e9
Данил Кочубей <kochubeidanil@gmail.com>
2023-12-14 22:21:07 +0700

    Задача 1

---
+++ app/src/main/java/com/eltex/androidschool/dao/EventsDaoImpl.kt
@@ -0,0 +1,113 @@
+package com.eltex.androidschool.dao
+
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import androidx.core.content.contentValuesOf
+import com.eltex.androidschool.model.Event
+
+class EventsDaoImpl(private val db: SQLiteDatabase) : EventsDao {
+    override fun getAll(): List<Event> {
+        val result = mutableListOf<Event>()
+
+        db.query(
+            EventsTable.TABLE_NAME,
+            EventsTable.allColumns,
+            null,
+            null,
+            null,
+            null,
+            "${EventsTable.ID} DESC"
+        ).use { cursor ->
+            while (cursor.moveToNext()) {
+                result += cursor.readEvent()
+            }
+        }
+
+        return result
+    }
+
+    override fun save(event: Event): Event {
+        val contentValues = contentValuesOf(
+            EventsTable.CONTENT to event.content,
+            EventsTable.AUTHOR to event.author,
+            EventsTable.PUBLISHED to event.published,
+            EventsTable.LIKE_BY_ME to event.likedByMe,
+            EventsTable.PARTICIPATE_BY_ME to event.participatedByMe,
+        )
+
+        if (event.id != 0L) {
+            contentValues.put(EventsTable.ID, event.id)
+        }
+
+        val id = db.replace(EventsTable.TABLE_NAME, null, contentValues)
+
+        return getEventById(id)
+    }
+
+    override fun likeById(id: Long): Event {
+        db.execSQL(
+            """
+            UPDATE ${EventsTable.TABLE_NAME} SET 
+                likedByMe = CASE WHEN likedByMe THEN 0 ELSE 1 END 
+            WHERE id = ?;
+        """.trimIndent(),
+            arrayOf(id.toString())
+        )
+
+        return getEventById(id)
+    }
+
+    private fun getEventById(id: Long): Event =
+        db.query(
+            EventsTable.TABLE_NAME,
+            EventsTable.allColumns,
+            "${EventsTable.ID} = ?",
+            arrayOf(id.toString()),
+            null,
+            null,
+            null,
+        ).use {
+            it.moveToFirst()
+            it.readEvent()
+        }
+
+    override fun deleteById(id: Long) {
+        db.delete(EventsTable.TABLE_NAME, "${EventsTable.ID} = ?", arrayOf(id.toString()))
+    }
+
+    override fun participateById(id: Long): Event {
+        db.execSQL(
+            """
+            UPDATE ${EventsTable.TABLE_NAME} SET 
+                participatedByMe = CASE WHEN participatedByMe THEN 0 ELSE 1 END 
+            WHERE id = ?;
+        """.trimIndent(),
+            arrayOf(id.toString())
+        )
+
+        return getEventById(id)
+    }
+
+    override fun editById(id: Long, content: String): Event {
+        db.execSQL(
+            """
+            UPDATE ${EventsTable.TABLE_NAME} SET 
+                content = ? 
+            WHERE id = ?;
+        """.trimIndent(),
+            arrayOf(content, id.toString())
+        )
+
+        return getEventById(id)
+    }
+
+    private fun Cursor.readEvent(): Event =
+        Event(
+            id = getLong(getColumnIndexOrThrow(EventsTable.ID)),
+            content = getString(getColumnIndexOrThrow(EventsTable.CONTENT)),
+            author = getString(getColumnIndexOrThrow(EventsTable.AUTHOR)),
+            published = getString(getColumnIndexOrThrow(EventsTable.PUBLISHED)),
+            likedByMe = getInt(getColumnIndexOrThrow(EventsTable.LIKE_BY_ME)) == 1,
+            participatedByMe = getInt(getColumnIndexOrThrow(EventsTable.PARTICIPATE_BY_ME)) == 1,
+        )
+}
---
+++ app/src/main/java/com/eltex/androidschool/db/DbHelper.kt
@@ -0,0 +1,27 @@
+package com.eltex.androidschool.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.eltex.androidschool.dao.EventsTable
+
+class DbHelper(context: Context) : SQLiteOpenHelper(context, "app_db", null, 1) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            """
+            CREATE TABLE ${EventsTable.TABLE_NAME} (
+                ${EventsTable.ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+                ${EventsTable.CONTENT} TEXT NOT NULL,
+                ${EventsTable.AUTHOR} TEXT NOT NULL,
+                ${EventsTable.PUBLISHED} TEXT NOT NULL,
+                ${EventsTable.LIKE_BY_ME} INTEGER NOT NULL DEFAULT 0,
+                ${EventsTable.PARTICIPATE_BY_ME} INTEGER NOT NULL DEFAULT 0
+            );
+        """.trimIndent()
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
